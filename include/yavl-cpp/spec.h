ae4539347eb5967f32c3aeadbbe102ead47e7270
Ruben Felgenhauer <Ruben.Felgenhauer@uni-hamburg.de>
2022-07-07 12:37:46 +0200

    Remove yaml-cpp include from generated headers

--- include/yavl-cpp/spec.h
+++ include/yavl-cpp/spec.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <yaml-cpp/yaml.h>
 #include "yavl-cpp/convert.h"
 
 #include <any>
--- src/yavl.cpp
+++ src/yavl.cpp
@@ -44,9 +44,7 @@ void CodeGenerator::emit_header() {
 void CodeGenerator::emit_includes() {
   outstream << "#pragma once" << std::endl << std::endl;
   if (is_emit_readers || is_emit_writers || is_emit_validator) {
-    outstream << "#include <yaml-cpp/yaml.h>" << std::endl
-              << "#include \"yavl-cpp/convert.h\"" << std::endl
-              << std::endl;
+    outstream << "#include \"yavl-cpp/convert.h\"" << std::endl << std::endl;
   }
   auto extra_includes = spec.ExtraIncludes;
   if (extra_includes.has_value()) {
