ee9df5f8f3cd7d459bcdef4d5809f8ec5a7bc6dc
Ruben Felgenhauer <Ruben.Felgenhauer@posteo.de>
2022-07-12 13:08:10 +0200

    Remove Namespace and Target from spec spec

--- include/yavl-cpp/spec.h
+++ include/yavl-cpp/spec.h
@@ -9,36 +9,8 @@
 #include <vector>
 #include "yaml-cpp/yaml.h"
 
-enum TargetType {
-  C,
-  CPP
-};
-
-inline void operator>>(const YAML::Node &input, TargetType &output) {
-  std::string tmp;
-  input >> tmp;
-  if (tmp == "C") {
-    output = C;
-  } else if (tmp == "CPP") {
-    output = CPP;
-  } else {
-    throw YAVL::BadConversionException(input, "TargetType");
-  }
-}
-
-inline YAML::Emitter& operator<<(YAML::Emitter &output, const TargetType &input) {
-  if (input == C) {
-    output << "C";
-  } else if (input == CPP) {
-    output << "CPP";
-  }
-  return output;
-}
-
 struct SpecType {
   std::optional<std::vector<std::string>> ExtraIncludes;
-  std::optional<std::string> Namespace;
-  std::optional<TargetType> Target;
   std::optional<std::tuple<std::string, std::string>> CustomCodeGenerator;
   tsl::ordered_map<std::string, YAML::Node> Types;
 };
@@ -46,8 +18,6 @@ struct SpecType {
 inline void operator>>(const YAML::Node &input, SpecType &output) {
   const std::unordered_map<std::string, bool> keys = {
     {"ExtraIncludes", false},
-    {"Namespace", false},
-    {"Target", false},
     {"CustomCodeGenerator", false},
     {"Types", true}
   };
@@ -63,8 +33,6 @@ inline void operator>>(const YAML::Node &input, SpecType &output) {
     }
   }
   input["ExtraIncludes"] >> output.ExtraIncludes;
-  input["Namespace"] >> output.Namespace;
-  input["Target"] >> output.Target;
   input["CustomCodeGenerator"] >> output.CustomCodeGenerator;
   input["Types"] >> output.Types;
 }
@@ -73,10 +41,6 @@ inline YAML::Emitter& operator<<(YAML::Emitter &output, const SpecType &input) {
   output << YAML::BeginMap;
   output << YAML::Key << "ExtraIncludes";
   output << YAML::Value << input.ExtraIncludes;
-  output << YAML::Key << "Namespace";
-  output << YAML::Value << input.Namespace;
-  output << YAML::Key << "Target";
-  output << YAML::Value << input.Target;
   output << YAML::Key << "CustomCodeGenerator";
   output << YAML::Value << input.CustomCodeGenerator;
   output << YAML::Key << "Types";
@@ -87,15 +51,12 @@ inline YAML::Emitter& operator<<(YAML::Emitter &output, const SpecType &input) {
 
 inline std::vector<std::string> get_types() {
   return {
-    "TargetType",
     "SpecType"
   };
 }
 
 inline std::tuple<bool, std::optional<std::string>> validate_simple(const YAML::Node &node, const std::string type_name) {
-  if (type_name == "TargetType") {
-    return validate<TargetType>(node);
-  } else if (type_name == "SpecType") {
+  if (type_name == "SpecType") {
     return validate<SpecType>(node);
   }
   return std::make_tuple(false, std::nullopt);
